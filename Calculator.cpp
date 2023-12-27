a0d80edc4cba5bd423ae0b804dccc2bb5ad48e07
Christian Encinares Bocz <144127538+xtianencinares@users.noreply.github.com>
2023-12-27 10:32:31 +0800

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char op;
+    double num1, num2;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    switch(op) {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+            } else {
+                cout << "Error: Division by zero is undefined." << endl;
+            }
+            break;
+        default:
+            cout << "Error: Invalid operator!" << endl;
+    }
+
+    return 0;
+}
